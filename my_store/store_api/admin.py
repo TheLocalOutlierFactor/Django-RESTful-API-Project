2978fa61afb2a2e38e3bf217a8df740da84a7ef6
TheLocalOutlierFactor <localoutlierfactor@gmail.com>
2024-10-01 19:40:27 +0500

    Register models in the admin panel

--- my_store/store_api/admin.py
+++ my_store/store_api/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Category
+
+
+class ProductCategoryInline(admin.TabularInline):
+    model = Product.categories.through
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductCategoryInline,
+    ]
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductCategoryInline,
+    ]
+    list_display = ['name', 'price', 'description']
+    list_filter = ['name']
+    search_fields = ['name']
+    exclude = ['categories']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
